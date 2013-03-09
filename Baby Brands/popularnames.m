ab30760ef2d41314c613db13eb2476429fd5d63c
drlucas <soccercoach1911@gmail.com>
2013-03-08 21:17:42 -0500

    populate table working

--- Baby Brands/popularnames.m
+++ Baby Brands/popularnames.m
@@ -10,6 +10,7 @@
 #import "FMDatabase.h"
 #import "FMResultSet.h"
 #import "DTCustomColoredAccessory.h"
+#import "names.h"
 
 
 @interface popularnames ()
@@ -22,16 +23,21 @@
     
     IBOutlet UIPickerView *yearlist;
     IBOutlet UISegmentedControl *sex;
+    IBOutlet UIButton *changeyearbtn;
+    
     NSInteger pickerrow;
     NSString *yearselect;
-
+    NSMutableArray *babynames;   //array to store male and female top 10 in each year
+    NSUserDefaults *defaults;
+    NSString* countrypref;
+    NSString *queryyear;
+    
 }
 
 @synthesize datesarray;   //array to store years to display in popup selector
 @synthesize yearlistcontainer; // to show/hide the years
 @synthesize datetable; // table with rank, count, babyname
-
-
+    
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -53,10 +59,12 @@
     self.navigationItem.titleView = label;
     label.text = NSLocalizedString(@"Name Popularity", @"");
     [label sizeToFit];
-    [self datesList]; // this will populate the popup button values
+    
     //NSLog(@"Value is %i", sex.selectedSegmentIndex); 0 is male, 1 is female
-    _yearselected.text =  @"1917"; //seed the default popup button with a value
-   
+    queryyear = @"2010";//seed the default popup button with a value
+    _yearselected.text = [NSString stringWithFormat:@"Top 15 names for %@", queryyear];
+    [self datesList]; // this will populate the popup button values
+    [self popularlist];
    
    // [self popularlist];
    // [datetable reloadData];
@@ -64,8 +72,8 @@
 
 -(NSMutableArray *) datesList {
     datesarray = [[NSMutableArray alloc] initWithCapacity:10];
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    NSString* countrypref = [defaults stringForKey:@"country_preference"];
+    defaults = [NSUserDefaults standardUserDefaults];
+    countrypref = [defaults stringForKey:@"country_preference"];
     // Create a FileManager object, we will use this to check the status of the database and to copy it over if required
 	NSString *databaseName;
 	NSString *databasePath;
@@ -90,13 +98,13 @@
     NSString *querystring;
     if ([countrypref isEqualToString:@"United States"]){
         //NSLog( @"Pref: %@",countrypref);
-        querystring= [NSString stringWithFormat:@"SELECT DISTINCT year from usa"];
+        querystring= [NSString stringWithFormat:@"SELECT DISTINCT year from usa order by year desc"];
         
     }
     if ([countrypref isEqualToString:@"Canada"])
     {
        // NSLog( @"Pref: %@",countrypref);
-        querystring= [NSString stringWithFormat:@"SELECT DISTINCT year from canada"];
+        querystring= [NSString stringWithFormat:@"SELECT DISTINCT year from canada order by year desc"];
     }
     results = [database executeQuery:querystring];
     int dateyear;
@@ -137,9 +145,11 @@
 {
     pickerrow = [yearlist selectedRowInComponent:0];
     yearselect = [[datesarray objectAtIndex:pickerrow] stringValue];
-    _yearselected.text =  yearselect;
-    //[self earnerList];
-    //[browsetable reloadData];
+    queryyear =  yearselect;
+    _yearselected.text = [NSString stringWithFormat:@"Top 15 names for %@", yearselect]; //seed the default popup button with a value
+
+    [self popularlist];
+    [datetable reloadData];
 }
 
 - (IBAction)hide40picker:(id)sender {
@@ -147,8 +157,11 @@
     [UIView setAnimationDuration:0.3];
     yearlistcontainer.frame = CGRectMake(0, 550, 320, 224);
     [UIView commitAnimations];
+    changeyearbtn.hidden = NO;
 }
 - (IBAction)changeyear:(id)sender {
+    //hide change year button
+    changeyearbtn.hidden = YES;
     [UIView beginAnimations:nil context:NULL];
     [UIView setAnimationDuration:0.3];
     yearlistcontainer.frame = CGRectMake(0, 300, 320, 224);
@@ -164,9 +177,101 @@
 - (IBAction)changesex:(id)sender {
     // NSLog(@"Value is %i", sex.selectedSegmentIndex);
     // do something if the sex changes (later on i'll do another reload of the table data)
+    [self popularlist];
+    [datetable reloadData];
 
 }
 
+
+
+-(NSMutableArray *) popularlist{
+    
+    babynames = [[NSMutableArray alloc] initWithCapacity:10];
+    // Create a FileManager object, we will use this to check the status of the database and to copy it over if required
+	NSString *databaseName;
+	NSString *databasePath;
+    BOOL success = 0;
+    databaseName = @"brandnames.sqlite";
+    defaults = [NSUserDefaults standardUserDefaults];
+    countrypref = [defaults stringForKey:@"country_preference"];
+	NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+	NSString *documentsDir = [documentPaths objectAtIndex:0];
+	databasePath = [documentsDir stringByAppendingPathComponent:databaseName];
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    
+	// Check if the database has already been created in the users filesystem
+	success = [fileManager fileExistsAtPath:databasePath];
+  	// If the database already exists then don't do anything
+	if(success) {// nothing to be done because....//NSLog(@"Database already exists");
+    }
+    else
+    {// If not then proceed to copy the database from the application to the users filesystem
+        NSString *databasePathFromApp = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
+        // Copy the database from the package to the users filesystem
+        [fileManager copyItemAtPath:databasePathFromApp toPath:databasePath error:nil];
+    };
+    // [self getyearsfromdb];
+    
+    // NSLog(@"User: %@",databasePath);
+    FMDatabase *database = [FMDatabase databaseWithPath:databasePath];
+    
+    [database open];
+    FMResultSet *results;
+    NSString *querystring;
+     //NSLog(@"Value is %i", sex.selectedSegmentIndex); 0 is male, 1 is female
+    //long theUnits = [defaults integerForKey:@"countrysetting"];
+    if ( [countrypref isEqualToString:@"United States"] )//usa
+    {
+        if (sex.selectedSegmentIndex == 0) { //male
+            querystring = [NSString stringWithFormat:@"SELECT firstname, sum(count) FROM usa where year = '%@' and sex = 'M' GROUP BY firstname order by sum(count) desc", queryyear];
+             NSLog(@"Selected sex: %@", querystring);
+            
+        }
+        if (sex.selectedSegmentIndex == 1) { //female
+            querystring = [NSString stringWithFormat:@"SELECT firstname, sum(count) FROM usa where year ='%@' and sex = 'F' GROUP BY firstname order by sum(count) desc", queryyear];
+        }
+        
+    }
+    if ( [countrypref isEqualToString:@"Canada"]){ //canada
+       
+        if (sex.selectedSegmentIndex == 0) {
+             
+            querystring = [NSString stringWithFormat:@"SELECT firstname, sum(count) FROM canada where year ='%@' and sex = 'M' GROUP BY firstname order by sum(count) desc", queryyear];
+        //NSLog(@"Selected sex: %@", querystring);
+        }
+        if (sex.selectedSegmentIndex == 1) {
+            querystring = [NSString stringWithFormat:@"SELECT firstname, sum(count) FROM canada where year ='%@' and sex = 'F' GROUP BY firstname order by sum(count) desc", queryyear];
+        }
+        
+    }
+    
+    results = [database executeQuery:querystring];
+    
+    while([results next]) {
+        babyname * babyn = [[babyname alloc] init];
+        //NSString *name = [results stringForColumn:@"name"];
+        babyn.firstname = [results stringForColumn:@"firstname"];
+        // babyn.sex = @"male";
+        babyn.frequency = [results intForColumn:@"sum(count)"];
+        // babyn.year = [results intForColumn:@"year"];
+        [babynames addObject:babyn];
+        //     onlyloop ++;
+        //   if (onlyloop == 11) break;
+    }
+    [database close];
+    // NSLog (@"Rows up: %i", [babynames count]);
+    return babynames;
+    
+    
+}
+
+
+
+-(void)tableViewSelectionDidChange:(NSNotification *)notification{
+    // NSLog(@"%d",[[notification object] selectedRow]);
+    // babyname* dataEntry = [babynames objectAtIndex:[[notification object] selectedRow]];
+    //displayname = dataEntry.firstname;
+}
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
@@ -174,34 +279,80 @@
     return 1;
 }
 
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    
-    if ([[UIScreen mainScreen] bounds].size.height == 568){
-        //4 inch screen
-     //   maxearners = 10;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section //this is the total number of names
+    {
+        
+        if ([[UIScreen mainScreen] bounds].size.height == 568){
+            //4 inch screen
+            return 15;
+            
+        }
+        else{
+            //3.5 inch screen
+            return 15;
+        }
+        
         
     }
-    else{
-        //3.5 inch screen
-     //   maxearners = 8;
+
+/*
+
+- (id)tableView:datetable objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)row {
+    babyname* dataEntry = [babynames objectAtIndex:row];
+    if([[aTableColumn identifier] isEqual:@"col1"])
+    {
+        //    NSLog(@"name: %@ column: %@ row: %i", dataEntry.firstname, [aTableColumn identifier], row);
+        return dataEntry.firstname;
     }
     
+ 
+ 
+ /*if([[aTableColumn identifier] isEqual:@"col2"])
+     {
+     // NSLog(@"name: %@ column: %@ row: %i", dataEntry.firstname, [aTableColumn identifier], row);
+     NSNumberFormatter *yearformatter = [[NSNumberFormatter alloc] init];
+     [yearformatter setNumberStyle:NSNumberFormatterNoStyle];
+     NSString *formattedOutput = [yearformatter stringFromNumber:[NSNumber numberWithInt:dataEntry.year]];
+     //NSLog(@"System free space: %@", formattedOutput);
+     return formattedOutput;
+     }*/
+/*
+
+if([[aTableColumn identifier] isEqual:@"col2"])
+    {
+        // NSLog(@"name: %@ column: %@ row: %i", dataEntry.firstname, [aTableColumn identifier], row);
+        NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+        [formatter setNumberStyle:NSNumberFormatterNoStyle];
+        NSString *formattedOutput = [formatter stringFromNumber:[NSNumber numberWithInt:dataEntry.frequency]];
+        return formattedOutput;
+    }
     
-    return 5;
+    if([[aTableColumn identifier] isEqual:@"col3"])
+    {
+        long myrow = row +1;
+        NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+        [formatter setNumberStyle:NSNumberFormatterNoStyle];
+        NSString *formattedOutput = [formatter stringFromNumber:[NSNumber numberWithInt:myrow]];
+        return formattedOutput;
+    }
+    
+    return @"nadda";
 }
+*/
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"browsecell";
+    static NSString *CellIdentifier = @"popularcell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];}
     cell.textLabel.textColor = [UIColor whiteColor];
     
-  //  Earner *earner = [theearners objectAtIndex:indexPath.row];
-    NSString *fullname = [NSString stringWithFormat:@"me"];
-    cell.textLabel.text = fullname;
+   // babyname * babyn = [[babyname alloc] init];
+    babyname* babyn = [babynames objectAtIndex:indexPath.row];
+  //  NSLog(@"Indexpath %i", indexPath);
+    NSString *fname = [NSString stringWithFormat:@"%@", babyn.firstname];
+    cell.textLabel.text = fname;
     
     //cell.detailTextLabel.text = earner.lastname;
     //cell.textLabel.text = [browsemenu objectAtIndex:indexPath.row];  //from original array
@@ -211,6 +362,30 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    cell.backgroundColor = [UIColor colorWithRed:0 green:0.188235 blue:0.313725 alpha:1];
+}
+
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+    //once i got the right row selected, then move over to the new view
+    /*
+     if (indexPath.row == 0) {
+     NSLog(@"Row Selected = %i",indexPath.row);
+     //[self performSegueWithIdentifier:@"ShowEarnerDetails" sender:self];
+     }
+     if (indexPath.row ==1 ) {
+     NSLog(@"Row Selected = %i",indexPath.row);
+     //
+     //[self performSegueWithIdentifier:@"search" sender:self];
+     }
+     if (indexPath.row ==2 ) {
+     //[self performSegueWithIdentifier:@"support" sender:self];
+     }
+     */
+}
+
 
 
 - (void)viewDidUnload {
@@ -218,6 +393,7 @@
     [self setYearlistcontainer:nil];
     yearlist = nil;
     [self setYearselected:nil];
+    changeyearbtn = nil;
     [super viewDidUnload];
 }
 @end
